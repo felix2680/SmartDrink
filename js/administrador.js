278f2aa013a23753218dca7eb93894400ad3860b
felix2680 <feliciano.hernandez@alumno.udg.mx>
2023-10-10 00:11:26 -0600

    añadi el formulario para insertar bebida

--- js/administrador.js
+++ js/administrador.js
@@ -1,12 +1,11 @@
 function mostrarVentana(ventanaId) {
-    // Oculta todas las ventanas
-    var ventanas = document.getElementsByClassName('content');
-    for (var i = 0; i < ventanas.length; i++) {
-      ventanas[i].style.display = 'none';
-    }
-  
-    // Muestra la ventana correspondiente
-    var ventana = document.getElementById(ventanaId);
-    ventana.style.display = 'block';
+  // Oculta todas las ventanas
+  var ventanas = document.getElementsByClassName('content');
+  for (var i = 0; i < ventanas.length; i++) {
+    ventanas[i].style.display = 'none';
   }
-  
+
+  // Muestra la ventana correspondiente
+  var ventana = document.getElementById(ventanaId);
+  ventana.style.display = 'block';
+}
