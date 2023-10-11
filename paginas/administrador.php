cc1aef2b64bc11ed50726318cdd73a690890e7b3
LuisCordero1512 <luis.cordero5942@alumnos.udg.mx>
2023-10-11 01:19:48 -0600

    Se agrego la funcion modificar bebidas

--- paginas/administrador.php
+++ paginas/administrador.php
@@ -10,6 +10,21 @@
 </head>
 
 <body>
+
+    <script src="../js/administrador.js"></script>
+    <?php
+    $mensaje_alerta = "";
+    include("../BD/sql.php");
+    ?>
+    <script>
+        <?php
+        // Mostrar mensaje de alerta si es necesario
+        if (!empty($mensaje_alerta)) {
+            echo "alert('$mensaje_alerta');";
+        }
+        ?>
+    </script>
+
     <div class="button-container">
         <button onclick="mostrarVentana('Insertar')">Insertar</button>
         <button onclick="mostrarVentana('Modificar')">Modificar</button>
@@ -32,26 +47,26 @@
             <!-- Contenido del formulario para la ventana de Insertar -->
         </form>
 
-        <form id="Modificar" class="content">
+        <form id="Modificar" class="content" method="post" enctype="multipart/form-data">
             <h2 class="mensaje">Modificar bebidas</h2>
-            <input type="text" name="nombre" placeholder="Nombre">
-            <input type="submit" name="buscar-modificar" placeholder="Buscar">
-            <input type="text" name="nombre" placeholder="Nombre">
-            <input type="text" name="categoria" placeholder="Categoria">
-            <input type="text" name="estacion" placeholder="Estación">
-            <input type="text" name="elaboracion" placeholder="Elaboración">
-            <input type="text" name="ingredientes" placeholder="Ingredientes">
-            <input type="file" name="imagen">
-            <input type="text" name="region" placeholder="Región">
-            <input type="text" name="tipo" placeholder="Tipo">
+            <input type="text" name="nombre_buscar" placeholder="Nombre">
+            <input type="submit" name="buscar-modificar" value="Buscar">
+            <input type="text" name="nombre" placeholder="Nombre" value="<?php echo isset($nombre) ? $nombre : ''; ?>">
+            <input type="text" name="categoria" placeholder="Categoria" value="<?php echo isset($categoria) ? $categoria : ''; ?>">
+            <input type="text" name="estacion" placeholder="Estacion" value="<?php echo isset($estacion) ? $estacion : ''; ?>">
+            <input type="text" name="elaboracion" placeholder="Elaboracion" value="<?php echo isset($elaboracion) ? $elaboracion : ''; ?>">
+            <input type="text" name="ingredientes" placeholder="Ingredientes" value="<?php echo isset($ingredientes) ? $ingredientes : ''; ?>">
+            <img id='imagen' src='data:image/jpeg;base64,<?php echo isset($imagen) ? $imagen : ''; ?>'>
+            <input type="text" name="region" placeholder="Region" value="<?php echo isset($region) ? $region : ''; ?>">
+            <input type="text" name="tipo" placeholder="Tipo" value="<?php echo isset($tipo) ? $tipo : ''; ?>">
             <input type="submit" name="modificar-bebida" value="Modificar Bebida">
-            <!-- Contenido del formulario para la ventana de Modificar -->
         </form>
 
         <form id="Eliminar" class="content" method="post" enctype="multipart/form-data">
             <h2 class="mensaje">Eliminar bebidas</h2>
             <input type="text" name="nombre_buscar" placeholder="Nombre de la bebida a eliminar">
             <input type="submit" name="eliminar-bebida" value="Eliminar Bebida">
+            <!-- Contenido del formulario para la ventana de Eliminar -->
         </form>
 
         <form id="Buscar" class="content">
@@ -61,19 +76,6 @@
         </form>
     </div>
 
-    <script src="../js/administrador.js"></script>
-    <?php
-    $mensaje_alerta = "";
-    include("../BD/sql.php");
-    ?>
-    <script>
-        <?php
-        // Mostrar mensaje de alerta si es necesario
-        if (!empty($mensaje_alerta)) {
-            echo "alert('$mensaje_alerta');";
-        }
-        ?>
-    </script>
 </body>
 
 </html>
