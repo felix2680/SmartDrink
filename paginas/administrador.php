9bbd9bbf80989453ca4329f05e649b2284fca801
LuisCordero1512 <luis.cordero5942@alumnos.udg.mx>
2023-10-11 13:57:53 -0600

    Se arreglo, la funcion modificar bebida

--- paginas/administrador.php
+++ paginas/administrador.php
@@ -34,18 +34,18 @@
 
         <form id="Modificar" class="content" method="post" enctype="multipart/form-data">
             <h2 class="mensaje">Modificar bebidas</h2>
-            <input type="text" name="nombre_buscar" placeholder="Nombre">
-            <input type="submit" name="buscar-modificar" placeholder="Buscar">
-            <input type="text" name="nombre" placeholder="Nombre">
-            <input type="text" name="categoria" placeholder="Categoria">
-            <input type="text" name="estacion" placeholder="Estación">
-            <input type="text" name="elaboracion" placeholder="Elaboración">
-            <input type="text" name="ingredientes" placeholder="Ingredientes">
+            <input type="text" name="nombre_buscar" placeholder="Nombre de la bebida a modificar">
+            <input type="submit" name="buscar-modificar" value="Buscar">
+            <input type="hidden" name="id" value="<?php echo isset($id) ? $id : ''; ?>">
+            <input type="text" name="nombre" placeholder="Nombre" value="<?php echo isset($nombre) ? $nombre : ''; ?>">
+            <input type="text" name="categoria" placeholder="Categoria" value="<?php echo isset($categoria) ? $categoria : ''; ?>">
+            <input type="text" name="estacion" placeholder="Estacion" value="<?php echo isset($estacion) ? $estacion : ''; ?>">
+            <input type="text" name="elaboracion" placeholder="Elaboracion" value="<?php echo isset($elaboracion) ? $elaboracion : ''; ?>">
+            <input type="text" name="ingredientes" placeholder="Ingredientes" value="<?php echo isset($ingredientes) ? $ingredientes : ''; ?>">
             <input type="file" name="imagen">
-            <input type="text" name="region" placeholder="Región">
-            <input type="text" name="tipo" placeholder="Tipo">
+            <input type="text" name="region" placeholder="Region" value="<?php echo isset($region) ? $region : ''; ?>">
+            <input type="text" name="tipo" placeholder="Tipo" value="<?php echo isset($tipo) ? $tipo : ''; ?>">
             <input type="submit" name="modificar-bebida" value="Modificar Bebida">
-            <!-- Contenido del formulario para la ventana de Modificar -->
         </form>
 
         <form id="Eliminar" class="content" method="post" enctype="multipart/form-data">
