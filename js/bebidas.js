e221b83c5f0680d006a7eadc33b215d003992698
felix2680 <feliciano.hernandez@alumno.udg.mx>
2023-11-18 02:07:39 -0600

    avances con la logica de la barra lateral

--- js/bebidas.js
+++ js/bebidas.js
@@ -1,6 +1,13 @@
 const opc = document.getElementById("opc");
 const barraLateral = document.querySelector(".barra-lateral");
 const spans = document.querySelectorAll("span");
+const palanca = document.querySelector(".switch");
+const circulo = document.querySelector(".circulo");
+palanca.addEventListener("click",()=>{
+    let body = document.body;
+    body.classList.toggle("dark-mode");
+    circulo.classList.toggle("prendido");
+});
 
 opc.addEventListener("click",() =>{
     barraLateral.classList.toggle("mini-barra-lateral");
