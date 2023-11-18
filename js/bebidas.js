63fa2ed981025a98d4008c84148bcc670571a257
felix2680 <feliciano.hernandez@alumno.udg.mx>
2023-11-18 15:39:55 -0600

    terminada la propuesta para la pagina

--- js/bebidas.js
+++ js/bebidas.js
@@ -3,17 +3,25 @@ const barraLateral = document.querySelector(".barra-lateral");
 const spans = document.querySelectorAll("span");
 const palanca = document.querySelector(".switch");
 const circulo = document.querySelector(".circulo");
-
-palanca.addEventListener("click",()=>{
+//let imagen = document.getElementById("imagenUsuario");
+/* lo que esta comentado es por si agregamos la funcionalidad
+   adelante*/
+palanca.addEventListener("click", () => {
     let body = document.body;
     body.classList.toggle("dark-mode");
     circulo.classList.toggle("prendido");
+
+ /*   if (body.classList.contains("dark-mode")) {
+       // imagen.src = "/img/usuario darkmode.png";
+    } else {
+        imagen.src = "/img/usuario.png";
+    }*/
 });
 
-opc.addEventListener("click",() =>{
+opc.addEventListener("click", () => {
     barraLateral.classList.toggle("mini-barra-lateral");
-    
-    spans.forEach((span)=>{
+
+    spans.forEach((span) => {
         span.classList.toggle("oculto");
     });
 
